cfbe07683991b60210ffa5c2d773ab4937aaf267
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-02-25 16:41:45 +0100

    added logout buttoon

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import { eventIndex } from './events/eventIndex.js'
 import { eventLogin } from './events/eventLogin.js'
 import { eventRegister } from './events/eventRegister.js'
 import { eventProfile } from './events/eventProfile.js'
+import storage from './storage/index.js'
 
 switch (location.pathname) {
   case '/' || '/index.html':
@@ -19,3 +20,18 @@ switch (location.pathname) {
   default:
     break
 }
+
+// all pages
+const token = storage.load('token')
+if (token) {
+  document.querySelector('#loginButton').style.display = 'none'
+  document.querySelector('#logoutButton').style.display = 'block'
+} else {
+  document.querySelector('#logoutButton').style.display = 'none'
+}
+// logout
+document.querySelector('#logoutButton').addEventListener('click', () => {
+  storage.remove('token')
+  storage.remove('user')
+  location.href = '/'
+})
