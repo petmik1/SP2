80445efe77fea4e3be36164df3af774f0381861a
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-02-28 23:28:29 +0100

    made the form collapse and display on press

--- src/js/render/createEditForm.js
+++ src/js/render/createEditForm.js
@@ -1,12 +1,18 @@
 export function createEditForm(post) {
   const form = document.querySelector('#editPost')
-  console.log(form)
-  form.style.display = 'flex'
-  form.classList.add('mx-auto', 'my-5')
-  form.title.value = post.title
-  form.description.value = post.description
-  if (post.media.length < 0) {
-    form.image.value = post.media[0]
+
+  if (form.style.display !== 'flex') {
+    form.style.display = 'flex'
+    form.classList.add('mx-auto', 'my-5')
+    form.title.value = post.title
+    form.description.value = post.description
+    if (post.media.length < 0) {
+      form.image.value = post.media[0]
+    }
+
+    return form
+  } else {
+    form.style.display = 'none'
+    return form
   }
-  return form
 }
