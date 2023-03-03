8f45d8d098f74def40304c3e5365e7fb57f38bea
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-03-03 18:10:47 +0100

    worked on the carrousel

--- src/js/render/indexCarruselPost.js
+++ src/js/render/indexCarruselPost.js
@@ -1,22 +1,37 @@
+import { countdown } from './countdown.js'
+
 export function indexCarruselPost(post, i) {
   const carousel = document.querySelector('.carousel-inner')
   const carousel_item = document.createElement('div')
   const carousel_caption = document.createElement('div')
   const current_bid = document.createElement('p')
   const timeLeft = document.createElement('p')
-  const bid = document.createElement('button')
+  const bid = document.createElement('a')
+  const more_container = document.createElement('div')
   const media = document.createElement('img')
   carousel_item.classList.add(`carousel-item`)
-  carousel_caption.classList.add(`carousel-caption`)
+  carousel_caption.classList.add(
+    `d-flex`,
+    `justify-content-around`,
+    `align-items-center`,
+    'carousel-caption'
+  )
+
   media.classList.add(`carousel-image`, `ratio`, `ratio-16x9`)
+  bid.classList.add(`btn`, `btn-primary`, 'flex-1')
+  timeLeft.classList.add(`flex-2`)
   if (i === 1) {
     carousel_item.classList.add(`active`)
   }
-  current_bid.innerText = post._count.bids
+
+  countdown(post.endsAt, timeLeft)
+  current_bid.innerText = 'Bids: ' + post._count.bids
   timeLeft.innerText = post.endsAt
-  bid.innerText = 'Bid'
+  bid.innerText = 'More'
+
   media.src = post.media[0]
-  carousel_caption.append(current_bid, timeLeft, bid)
+  more_container.append(bid)
+  carousel_caption.append(current_bid, timeLeft, more_container)
   carousel_item.append(media)
   carousel_item.append(carousel_caption)
   carousel.append(carousel_item)
