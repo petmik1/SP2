8bedd56d37918b370837d64a94b582c4618b350f
Petter Emil Hatland Mikalsen <petter.mikalsen.pehm@gmail.com>
2023-03-04 15:44:51 +0100

    added filters to index page

--- src/js/events/eventIndex.js
+++ src/js/events/eventIndex.js
@@ -10,4 +10,17 @@ export async function eventIndex() {
   // Creating cards
   const cardsData = await getPosts(100, 'created', 'desc', 'false')
   CardsPosts(cardsData)
+
+  // Creating event for filters
+  const form = document.querySelector('#filters')
+  form.addEventListener('click', async (e) => {
+    const sortOrder = form.sortOrder.value
+    const sortBy = form.sortBy.value
+    const active = form.active.value
+    const cards = document.querySelector('.cards')
+    cards.innerHTML = ''
+    console.log(100, sortBy, sortOrder, active)
+    const cardsData = await getPosts(100, sortBy, sortOrder, active)
+    CardsPosts(cardsData)
+  })
 }
